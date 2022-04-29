9e6c7b2ecd942570b8fbfe809357eb726e0446cb
m2lovric <m2lovric@gmail.com>
2022-04-29 02:07:55 +0200

    Add react router

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,5 @@
 import * as React from 'react';
 import {
-  ChakraProvider,
   Box,
   Text,
   Button,
@@ -14,43 +13,42 @@ import logo from './assets/Logo.svg';
 import airbnb from './assets/Airbnb.svg';
 import booking from './assets/Booking.svg';
 import plum from './assets/Plum Guide.svg';
-import lightTheme from './theme';
 import check from './assets/Checkmark.svg';
+import { Link } from 'react-router-dom';
 
 export const App = () => (
-  <ChakraProvider theme={lightTheme}>
-    <HStack>
-      <Box w='55%' h='100vh' p='10%'>
-        <Box w='80%'>
-          <Image src={logo} alt='logo' marginBottom={20}></Image>
-          <Heading as='h1' size='3xl'>
-            Explore the best camps on Earth.
-          </Heading>
-          <Text marginY={6} fontSize='lg' color='blackAlpha.700'>
-            Yelpcamp is a curated list of the best camping spots on Earth.
-            Unfiltered and unbiased reviews
-          </Text>
-          <VStack
-            h='100px'
-            alignItems='flex-start'
-            justifyContent='space-between'
-            marginBottom={6}
-            color='blackAlpha.700'
-          >
-            <HStack>
-              <Image src={check}></Image>
-              <Text>Add your own camp suggestions.</Text>
-            </HStack>
-            <HStack>
-              <Image src={check}></Image>
-              <Text>Leave reviews and experiences.</Text>
-            </HStack>
-            <HStack>
-              <Image src={check}></Image>
-              <Text>See locations for all camps.</Text>
-            </HStack>
-          </VStack>
-
+  <HStack overflowY='hidden'>
+    <Box w='55%' h='100vh' p='10%'>
+      <Box w='80%'>
+        <Image src={logo} alt='logo' marginBottom={20}></Image>
+        <Heading as='h1' size='3xl'>
+          Explore the best camps on Earth.
+        </Heading>
+        <Text marginY={6} fontSize='lg' color='blackAlpha.700'>
+          Yelpcamp is a curated list of the best camping spots on Earth.
+          Unfiltered and unbiased reviews
+        </Text>
+        <VStack
+          h='100px'
+          alignItems='flex-start'
+          justifyContent='space-between'
+          marginBottom={6}
+          color='blackAlpha.700'
+        >
+          <HStack>
+            <Image src={check}></Image>
+            <Text>Add your own camp suggestions.</Text>
+          </HStack>
+          <HStack>
+            <Image src={check}></Image>
+            <Text>Leave reviews and experiences.</Text>
+          </HStack>
+          <HStack>
+            <Image src={check}></Image>
+            <Text>See locations for all camps.</Text>
+          </HStack>
+        </VStack>
+        <Link to='/search'>
           <Button
             backgroundColor='blackAlpha.900'
             color='white'
@@ -61,19 +59,19 @@ export const App = () => (
           >
             View Campgrounds
           </Button>
-          <Text marginTop={6} color='blackAlpha.700'>
-            Partnered with:
-          </Text>
-          <HStack>
-            <Image src={airbnb}></Image>
-            <Image src={booking}></Image>
-            <Image src={plum}></Image>
-          </HStack>
-        </Box>
+        </Link>
+        <Text marginTop={6} color='blackAlpha.700'>
+          Partnered with:
+        </Text>
+        <HStack>
+          <Image src={airbnb}></Image>
+          <Image src={booking}></Image>
+          <Image src={plum}></Image>
+        </HStack>
       </Box>
-      <Box w='45%' h='100vh' m={0}>
-        <Image src={hero} objectFit='cover' w='100%' h='auto'></Image>
-      </Box>
-    </HStack>
-  </ChakraProvider>
+    </Box>
+    <Box w='45%' h='100vh' m={0}>
+      <Image src={hero} w='100%' h='auto'></Image>
+    </Box>
+  </HStack>
 );
--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,19 @@
 import * as React from 'react';
 import ReactDOM from 'react-dom';
 import { App } from './App';
-import { ThemeProvider } from '@chakra-ui/react';
+import Search from './Search';
+import { ChakraProvider } from '@chakra-ui/react';
 import lightTheme from './theme';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <ThemeProvider theme={lightTheme}>
-      <App />
-    </ThemeProvider>
-  </React.StrictMode>,
+  <BrowserRouter>
+    <ChakraProvider theme={lightTheme}>
+      <Routes>
+        <Route path='/' element={<App />} />
+        <Route path='/search' element={<Search />} />
+      </Routes>
+    </ChakraProvider>
+  </BrowserRouter>,
   document.getElementById('root')
 );
